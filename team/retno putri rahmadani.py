7aa05fda38b9d86b6dcaa204f34d29031372aad6
wildanadji879 <wildanajiku@gmail.com>
2025-02-19 23:59:04 +0700

    Add files via upload

---
+++ team/retno putri rahmadani.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran) :
+    print(f"Halo! Perkenalkan, nama saya {nama}")
+    print(f"NIM: {nim}")
+    print(f"Peran saya yaitu: {peran}")
+
+if _ _name_ _ ## "_ _main":
+    nama = "Retno Putri Rahmadani"
+    nim = "22.130.10.358"
+    peran = "Perancangan"
+
+    perkenalan(nama, nim, peran)
