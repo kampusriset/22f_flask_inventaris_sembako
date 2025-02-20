c0529ac88b08902eb006947d91a18c76e281db24
wildanadji879 <wildanajiku@gmail.com>
2025-02-20 16:47:03 +0700

    Add files via upload

---
+++ team/Cikal Oktakvianurjanah Qomar.py.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print(f"Halo! Perkenalkan, nama saya {nama}")
+    print(f"NIM: {nim}")
+    print(f"Peran saya yaitu: {peran}")
+
+if __name__ == "__main__":
+    nama = "Cikal Oktavinurjanah Qomar"
+    nim = "22.130.10.351"
+    peran = "Pengembang Bagian Anggota"
+    
+    perkenalan(nama, nim, peran)
