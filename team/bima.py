93a6867d523b03b52a2efd327555fd895e163aad
Bima500 <bima.10439@mhs.amikomsolo.ac.id>
2025-02-20 20:02:40 +0700

    create bima.py

--- team/bima.py
+++ team/bima.py
@@ -6,5 +6,5 @@ def perkenalan(nama, nim, peran):
 if __name__ == "__main__":
     nama = "Bima"
     nim = "22.130.10.439"
-    peran = "Membantu membuat fitur"
+    peran = "Developer 2"
     perkenalan(nama, nim, peran)
