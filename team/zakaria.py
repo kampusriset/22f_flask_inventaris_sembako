a0f47d7f98b8cd98e8968a082de56a86d9004e69
Bima500 <bima.10439@mhs.amikomsolo.ac.id>
2025-02-20 19:51:27 +0700

    Create zakaria.py

---
+++ team/zakaria.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print(f"Halo! Perkenalkan, nama saya {nama}")
+    print(f"NIM: {nim}")
+    print(f"Peran saya yaitu: {peran}")
+
+if __name__ == "__main__":
+    nama = "Muhammad Zakaria Fanandi"
+    nim = "22.130.10.145"
+    peran = "Anggota"
+
+    perkenalan(nama, nim, peran)
